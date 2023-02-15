db4937b54fef313f823b4235a92c186c7181d443
davidmutxo <davidesigner.web@gmail.com>
2023-02-15 15:12:21 +0000

    chages

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 // Bag
 let bagIcon = document.querySelector('#bag-icon')
-let bagIcon = document.querySelector('#bag-icon')
-let bagIcon = document.querySelector('#bag-icon')
+let bag = document.querySelector('.bag')
+let closeBag = document.querySelector('#close-bag')
+
+//open bag
+bagIcon.onclick = () => {
+    bag.classList.add("active");
+};
+closeBag.onclick = () => {
+    bag.classList.remove("active");
+};
