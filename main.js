859b94e38242d604983993af782bcb0f402c7cf0
davidmutxo <davidesigner.web@gmail.com>
2023-02-23 21:57:15 +0000

    bag

--- main.js
+++ main.js
@@ -67,20 +67,17 @@ function addProducttoBag(title, price, producImg) {
 
 
 var bagBoxContent = `
-<img src="image/Shop/SeekPng.com_salt-png_9189044.png" alt="">
-<div class="details-box">
-  <div class="bag-product-title">YEEZY 500 'SALT'</div>
-  <div class="bag-price">€0</div>
-</div>
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash bag-remove" viewBox="0 0 16 16"> <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/> <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/> </svg>`;
+            <img src="image/Shop/SeekPng.com_salt-png_9189044.png" alt="">
+            <div class="details-box">
+                <div class="bag-product-title">YEEZY 500 'SALT'</div>
+                <div class="bag-price">$100</div>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash bag-remove" viewBox="0 0 16 16"> <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/> <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/> </svg>`;
 bagShopBox.innerHTML = bagBoxContent;
 bagItems.append(bagShopBox);
 bagShopBox.getElementsByClassName('bag-remove')[0].addEventListener('click',removeBagItem);
 
 }
-
-
-
 
 
 
@@ -94,9 +91,9 @@ function updatetotal (){
     for (var i = 0; i < bagBoxes.length; i++){
         var bagBox = bagBoxes[i];
         var priceElement = bagBox.getElementsByClassName('bag-price') [0];
+        var quantity = quantityElement.value
         var price = parseFloat(priceElement.innerText.replace ('$',''));
-        total = total + (price * quantity)
+        total = total + (price * quantity);
         document.getElementsByClassName ('total-price')[0].innerText = '$' + total;
-
     }
 }
